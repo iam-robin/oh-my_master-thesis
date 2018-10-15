01f500907aa93c763e053175199dc6a8cba91d1f
Robin Spielmann <spielmannrobin@gmail.com>
2018-10-15 15:49:32 +0200

    output percentage limit on new tab page

--- src/newtab/router/pages/Page4.vue
+++ src/newtab/router/pages/Page4.vue
@@ -3,9 +3,11 @@
   <ul>
     <li v-for="limit in limits" :key="limit.domain"
     :style="{ 'order': limit.timeLimit}">
-      <span class="domain">{{ limit.domain }}</span>
+      <span>{{ limit.domain }}</span>
       <span class="line"></span>
-      <span>{{ limit.timeLimit }} min</span>
+      <span>{{ limit.timeLimit }}min</span>
+      <span v-if="limit.percentageLimit <= 100" class="percentage">({{ round(limit.percentageLimit, 2) }}%)</span>
+      <span v-else class="limit-reached">(100%)</span>
     </li>
   </ul>
 </div>
@@ -27,6 +29,42 @@ export default {
     getLimits() {
       let limits = JSON.parse(localStorage.getItem('limits'));
       this.limits = limits;
+      this.getLimitPercentage();
+    },
+    getLimitPercentage() {
+      let today = this.getTodaysDate();
+      let websites = JSON.parse(localStorage.getItem(today));
+
+      for (let i = 0; i < websites.length; i++) {
+        for (let x = 0; x < this.limits.length; x++) {
+          if (websites[i].domain === this.limits[x].domain) {
+            let usageTime = websites[i].time;
+            let limitTime = this.limits[x].timeLimit * 60 * 1000;
+            let limitPercentage = (100 / limitTime) * usageTime;
+            this.limits[x]['percentageLimit'] = limitPercentage;
+          }
+        }
+      }
+    },
+    getTodaysDate: function() {
+      let today = new Date();
+      let dd = today.getDate();
+      let mm = today.getMonth() + 1;
+      let yyyy = today.getFullYear();
+
+      if (dd < 10) {
+        dd = '0' + dd;
+      }
+
+      if (mm < 10) {
+        mm = '0' + mm;
+      }
+
+      today = dd + '/' + mm + '/' + yyyy;
+      return today;
+    },
+    round: function(value, decimals) {
+      return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
     },
   },
 };
@@ -53,8 +91,8 @@ export default {
       justify-content: space-between;
       align-items: center;
 
-      .domain {
-        width: 100px;
+      .limit {
+        text-align: right;
       }
 
       .line {
@@ -62,6 +100,15 @@ export default {
         background-color: #e6e9ee;
         margin: 0 24px;
         flex-grow: 99;
+      }
+
+      .percentage {
+        margin-left: 8px;
+      }
+
+      .limit-reached {
+        margin-left: 8px;
+        color: #f53b57;
       }
     }
   }
