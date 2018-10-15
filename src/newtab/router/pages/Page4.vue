c7401669b60d632abc11302b04f3323e90b2f9d6
Robin Spielmann <spielmannrobin@gmail.com>
2018-10-15 14:29:38 +0200

    limit new tab route

--- src/newtab/router/pages/Page4.vue
+++ src/newtab/router/pages/Page4.vue
@@ -1,8 +1,11 @@
 <template>
 <div class="container">
   <ul>
-    <li v-for="limit in limits" :key="limit.domain">
-      <span>{{ limit.domain }}</span><span>{{ limit.timeLimit }}</span>
+    <li v-for="limit in limits" :key="limit.domain"
+    :style="{ 'order': limit.timeLimit}">
+      <span class="domain">{{ limit.domain }}</span>
+      <span class="line"></span>
+      <span>{{ limit.timeLimit }} min</span>
     </li>
   </ul>
 </div>
@@ -32,5 +35,35 @@ export default {
 <style lang="scss" scoped>
 .container {
   margin-top: 240px;
+
+  ul {
+    width: 40%;
+    margin-bottom: 160px;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: flex-start;
+    padding: 0;
+    margin: 0 auto;
+
+    li {
+      flex: 0 0 100%;
+      display: flex;
+      font-size: 12px;
+      padding: 8px 0;
+      justify-content: space-between;
+      align-items: center;
+
+      .domain {
+        width: 100px;
+      }
+
+      .line {
+        height: 1px;
+        background-color: #e6e9ee;
+        margin: 0 24px;
+        flex-grow: 99;
+      }
+    }
+  }
 }
 </style>
