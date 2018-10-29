def6faf5db0f5d332bc817833c3dd3f71d30c79b
Robin Spielmann <spielmannrobin@gmail.com>
2018-10-29 08:38:17 +0100

    changed icons

--- src/newtab/App.vue
+++ src/newtab/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div>
-
-    <div class="menu">
-      <router-link to="/page1">
-        01 Aufrufe
-      </router-link>
-      <router-link to="/page2">
-        02 Dauer
-      </router-link>
-      <router-link to="/page3">
-        03 Ratio
-      </router-link>
-      <router-link to="/page4">
-        04 Limits
-      </router-link>
+    <div class="info">
+      <h1>fixed</h1>
+      <!--<div class="menu">
+        <router-link to="/page1">
+          01 Aufrufe
+        </router-link>
+        <router-link to="/page2">
+          02 Dauer
+        </router-link>
+        <router-link to="/page3">
+          03 Ratio
+        </router-link>
+        <router-link to="/page4">
+          04 Limits
+        </router-link>
+      </div>-->
     </div>
-
-    <router-view></router-view>
+    <div class="content">
+      <h1>variable</h1>
+      <!--<router-view></router-view>-->
+    </div>
   </div>
 </template>
 
@@ -27,13 +31,24 @@ body {
   padding: 0;
   font-family: 'Fira Mono', monospace;
 
+  .info {
+    width: 40%;
+    position: fixed;
+    background-color: #e5e5e5;
+    height: 100vh;
+  }
+
+  .content {
+    display: inline-block;
+    margin-left: 40%;
+    width: 60%;
+    background-color: #fece60;
+    min-height: 8000px;
+  }
+
   .menu {
-    position: absolute;
-    top: 80px;
-    left: 0;
-    width: 100%;
     display: flex;
-    justify-content: center;
+    justify-content: left;
 
     a {
       padding: 0 24px;
